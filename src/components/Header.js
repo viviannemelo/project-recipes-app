cb509b6b21096a7a7858712223b41553743db0b5
GuilhermeFMP <guilhermemoraispinho@gmail.com>
2023-01-25 19:36:31 -0300

    Adicionad os botões de Profile e Habilitar o input

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import ProfileIcon from '../images/profileIcon.svg';
 import SearchIcon from '../images/searchIcon.svg';
 
@@ -7,6 +8,7 @@ function Header(props) {
   const [nameHeader, setNameHeader] = useState('');
   const [profile, setProfile] = useState(false);
   const [search, setSearch] = useState(false);
+  const [showInput, setShowInput] = useState(false);
 
   useState(() => {
     const { iconSearch, iconProfile, name } = props;
@@ -14,21 +16,41 @@ function Header(props) {
     setProfile(iconProfile);
     setNameHeader(name);
   }, []);
+
+  const ableInput = () => {
+    setShowInput(showInput === false);
+  };
+
   return (
     <header>
       <h1 data-testid="page-title">{ nameHeader }</h1>
       { profile
-      && <img
-        data-testid="profile-top-btn"
-        src={ ProfileIcon }
-        alt="profile icon"
-      />}
+      && (
+        <Link to="/profile">
+          <img
+            data-testid="profile-top-btn"
+            src={ ProfileIcon }
+            alt="profile icon"
+          />
+        </Link>
+      )}
       { search
-      && <img
-        data-testid="search-top-btn"
-        src={ SearchIcon }
-        alt="search icon"
-      />}
+      && (
+        <button
+          onClick={ ableInput }
+        >
+          <img
+            data-testid="search-top-btn"
+            src={ SearchIcon }
+            alt="search icon"
+          />
+        </button>
+      )}
+      { showInput
+        && <input
+          data-testid="search-input"
+          type="text"
+        />}
     </header>
   );
 }
