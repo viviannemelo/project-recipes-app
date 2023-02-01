1e06a92d0948de796674bfa72d54b8ff97672ae5
alexsousa83 <alex.sousa.araujo@hotmail.com>
2023-02-01 18:29:59 -0300

    req 57 ate 62

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import ProfilePage from '../components/ProfilePage';
 
 function Profile() {
   return (
     <div>
       <Header name="Profile" iconProfile iconSearch={ false } />
+      <ProfilePage />
       <Footer />
     </div>
   );
