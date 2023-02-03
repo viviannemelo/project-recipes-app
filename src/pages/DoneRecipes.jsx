59dc8e8cfb6b4d76fa8cec7fb185d6a997e68b0f
GuilhermeFMP <guilhermemoraispinho@gmail.com>
2023-02-03 01:18:03 -0300

    Finalizado Tela de Receitas Finalizadas Req 44 a 49

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Done from '../components/Done';
 import Header from '../components/Header';
 
 function DoneRecipes() {
   return (
     <div>
       <Header name="Done Recipes" iconProfile iconSearch={ false } />
+      <Done />
     </div>
   );
 }
